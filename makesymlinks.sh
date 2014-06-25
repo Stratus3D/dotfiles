9c31be242217e90b40d6e66fd8209add81c863bc
Trevor Brown <admin@stratus3d.com>
2014-06-25 08:23:16 -0400

    Added jshintrc.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="vimrc vim zshrc tmux.conf gitconfig gitignore_global ackrc"
+files="vimrc vim zshrc tmux.conf gitconfig gitignore_global ackrc jshintrc"
 
 ##########
 

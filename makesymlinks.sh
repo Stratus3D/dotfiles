d4ed9a9364d16760f154a84569f7ef7b97213d6f
Trevor Brown <admin@stratus3d.com>
2014-08-22 19:26:12 -0400

    Updated the makesymlinks script so the output is cleaner. Also fixed two bugs.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,13 +6,16 @@
 
 ########## Variables
 
+border="====="
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="vimrc vim zshrc bashrc aliases grep path tmux.conf gitconfig \
-    gitignore_global ackrc ctags jshintrc"
+files="vimrc vim zshrc bashrc aliases grep path tmux.conf gitconfig gitignore_global ackrc ctags jshintrc"
 
 ##########
+
+# Remove everything in dotfiles_old
+rm -rf $olddir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -25,9 +28,9 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  mv ~/.$file ~/dotfiles_old/
+  mv ~/.$file $olddir
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
@@ -39,6 +42,12 @@ fi
 # link the default tmuxinator project
 ln -s $dir/tmuxinator/default.yml ~/.tmuxinator/default.yml
 
+# If vundle is already installed, remove it and fetch the latest from Github
+if [ -d ~/dotfiles/vim/bundle/Vundle.vim ]; then
+    rm -rf ~/dotfiles/vim/bundle/Vundle.vim
+fi
 # download vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+echo "$border Linking complete! $border"

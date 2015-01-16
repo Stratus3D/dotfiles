a2303220068e6a6da44579faff047ccd4e2d2c1b
Trevor Brown <admin@stratus3d.com>
2015-01-16 15:18:24 -0500

    More work on the setup.sh script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@
 # Terminate script if anything exits with a non-zero value
 set -e
 
+############################
+# Install software on laptop
+############################
 # Get the uname string
 unamestr=`uname`
 
@@ -15,11 +18,23 @@ if [[ "$unamestr" == 'Darwin' ]]; then
     curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/master/mac
     sh mac 2>&1 | tee ~/laptop.log
 elif [[ "$unamestr" == 'Linux' ]]; then
-    # Do nothing for now, add custom code here
-    echo "script for Linux incomplete"
+    # Do nothing for now
+    # TODO: write code to install software on linux
+    echo "Script for Linux incomplete. Please install all the software manually."
 fi
 
-# download the dotfiles
+############################
+# Create commonly used directories
+############################
+mkdir ~/Development
+mkdir ~/Development/src # Go directories
+mkdir ~/Development/bin
+mkdir ~/Documentation
+mkdir ~/Installers
+
+############################
+# Setup dotfiles
+############################
 cd ~
 git clone https://github.com/Stratus3D/dotfiles.git
 cd dotfiles/

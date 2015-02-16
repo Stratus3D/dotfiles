40ed36b84c33a971124e9bb41e3ce2702f4bd48b
Trevor Brown <admin@stratus3d.com>
2015-02-16 09:15:17 -0500

    Create platform_name.sh script.

---
+++ tools/platform_name.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Provide the name of the current operating system.
+
+set -u # Prevent unset variables
+set -e # Stop on an error
+
+# Default to 'unknown'
+platform='unknown'
+unamestr=$(uname)
+
+if [[ "$unamestr" == 'Linux' ]]; then
+    platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+    platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    platform='darwin'
+fi
+
+echo $platform

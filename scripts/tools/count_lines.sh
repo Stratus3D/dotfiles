912dd17074f9bd7ff353ed72a1e1309e2ad5d5a1
Trevor Brown <admin@stratus3d.com>
2015-05-19 10:31:09 -0400

    Update the count_lines.sh script.

--- scripts/tools/count_lines.sh
+++ scripts/tools/count_lines.sh
@@ -1,3 +1,15 @@
 #! /bin/bash -
+
+# Check if the file extension was provided
+if [[ $# < 2 ]]; then
+    cat <<EOF
+    Usage: `basename "$0"` [file_extension]
+EOF
+    exit 1;
+fi
+
+# If we get past the if condition we find all files with the given extension
 FILES=$(find . -iname "*.$1")
+
+# Then we count the lines in all those files
 $FILES | wc -l

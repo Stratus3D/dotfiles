9e1421ba8e5d5ae67a871b7580ffe6b49cafd5a6
Stratus3D <admin@stratus3d.com>
2015-05-28 06:55:40 -0400

    Remove utf-8 symbol literals from checkenv.sh.

--- scripts/checkenv.sh
+++ scripts/checkenv.sh
@@ -1,37 +1,44 @@
 #!/bin/bash -
 
+# TODO: Determine if this script is needed. If it is fix formatting issues
+
+# Instead of using literal UTF-8 characters for the checkmark and x, we use
+# these variables
+CHECKMARK="\xe2\x9c\x94"
+FAIL="\xe2\x9c\x94"
+
 # Functions ==============================================
 
 # return 1 if global command line program installed, else 0
 # example
 # echo "node: $(program_is_installed node)"
 function program_is_installed {
-# set to 1 initially
-local return_=1
-# set to 0 if not found
-type $1 >/dev/null 2>&1 || { local return_=0; }
-# return value
-echo "$return_"
-        }
+    # set to 1 initially
+    local return_=1
+    # set to 0 if not found
+    type $1 >/dev/null 2>&1 || { local return_=0; }
+    # return value
+    echo "$return_"
+}
 
-        # return 1 if local npm package is installed at ./node_modules, else 0
-        # example
-        # echo "gruntacular : $(npm_package_is_installed gruntacular)"
-        function npm_package_is_installed {
-        # set to 1 initially
-        local return_=1
-        # set to 0 if not found
-        ls node_modules | grep $1 >/dev/null 2>&1 || { local return_=0; }
-        # return value
-        echo "$return_"
-    }
+# return 1 if local npm package is installed at ./node_modules, else 0
+# example
+# echo "gruntacular : $(npm_package_is_installed gruntacular)"
+function npm_package_is_installed {
+    # set to 1 initially
+    local return_=1
+    # set to 0 if not found
+    ls node_modules | grep $1 >/dev/null 2>&1 || { local return_=0; }
+    # return value
+    echo "$return_"
+}
 
-    # display a message in red with a cross by it
-    # example
-    # echo echo_fail "No"
-    function echo_fail {
+# display a message in red with a cross by it
+# example
+# echo echo_fail "No"
+function echo_fail {
     # echo first argument in red
-    printf "\e[31m✘ ${1}"
+    printf "\e[31m$FAIL ${1}"
     # reset colours back to normal
     echo -e "\033[0m"
 }
@@ -41,7 +48,7 @@ echo "$return_"
 # echo echo_fail "Yes"
 function echo_pass {
 # echo first argument in green
-printf "\e[32m✔ ${1}"
+printf "\e[32m$CHECKMARK ${1}"
 # reset colours back to normal
 echo -e "\033[0m"
                         }

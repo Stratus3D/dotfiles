0be2edcf9336df885b62dbcded13ce9eed286c20
Stratus3D <admin@stratus3d.com>
2015-07-22 07:09:45 -0400

    Complete lua installation script.

--- scripts/setup/install/lua.sh
+++ scripts/setup/install/lua.sh
@@ -1,20 +1,36 @@
 #!/bin/bash -
+#
+# Usage ./lua.sh [macosx|linux]
+#
+TARGET=${1:-linux}
+FILE="lua-5.3.1"
+TAR_GZ_FILE="$FILE.tar.gz"
+INSTALL_DIR="$HOME/lib/lua"
 
-TARGET=${TARGET:-linux}
-FILE="lua-5.3.1.tar.gz"
-INSTALL_DIR="$HOME/lib"
+echo "Preparing lua directory..."
+if [ ! -d "$INSTALL_DIR" ]; then
+    mkdir $INSTALL_DIR
+fi
 
-echo "Installing lua..."
+echo "Installing lua ($FILE)..."
 
 # Prepare to install
 cd $INSTALL_DIR
 
 # Download and uncompress
-wget "http://www.lua.org/ftp/$FILE"
-tar -zxvf $FILE
+wget "http://www.lua.org/ftp/$TAR_GZ_FILE"
+tar -zxvf $TAR_GZ_FILE
 
 # Build Lua
+cd "$INSTALL_DIR/$FILE"
 make $TARGET test local
 
+# Symlink to lua/current
+echo "Symlinking $FILE/install to lua/current/..."
+ln -s "$INSTALL_DIR/$FILE/install" "$INSTALL_DIR/current"
+
 # Clean up
-rm $FILE
+echo "Cleaning up..."
+cd $INSTALL_DIR
+rm $TAR_GZ_FILE
+echo "Installation complete"

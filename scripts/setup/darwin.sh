140b8f16546978a38ee3fcdd0d30d8ac9403781a
Stratus3D <admin@stratus3d.com>
2015-07-22 07:16:29 -0400

    Update darwin install script so lua script is run.

--- scripts/setup/darwin.sh
+++ scripts/setup/darwin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -
 
+# Get the directory this script is stored in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+"$DIR/install/lua_darwin.sh"
 # TODO: write code to install software on Darwin
 echo "Script for Darwin incomplete. Please install all the software manually."

e4fd002bf08850eafc68d07e394eb5ea82f6e5d9
Stratus3D <admin@stratus3d.com>
2015-07-22 07:18:21 -0400

    Update linux script to include lua installation.

--- scripts/setup/linux.sh
+++ scripts/setup/linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -
 
+# Get the directory this script is stored in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+"$DIR/install/lua_linux.sh"
 # TODO: write code to install software on Linux
 echo "Script for Linux incomplete. Please install all the software manually."

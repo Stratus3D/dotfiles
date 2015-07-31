811bd09bc8d1b1f0edd52d060483b460cd245619
Stratus3D <admin@stratus3d.com>
2015-07-30 20:37:19 -0400

    Add rsync-exclude file.

--- scripts/makesymlinks.sh
+++ scripts/makesymlinks.sh
@@ -16,7 +16,7 @@ dotfiles=$HOME/dotfiles               # dotfiles directory
 olddir=$HOME/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="vimrc vim zshrc bashrc tmux.conf gitconfig gitignore_global ackrc ctags
-screenrc jshintrc irssi"
+screenrc jshintrc irssi rsync-exclude"
 
 # Remove everything in dotfiles_old
 rm -rf $olddir

61d7f892e2cff1a1b538843bf0e4da5ecf5e955e
Stratus3D <admin@stratus3d.com>
2015-08-03 07:14:15 -0400

    Update darwin setup script to use local laptop script.

--- scripts/setup/darwin.sh
+++ scripts/setup/darwin.sh
@@ -3,6 +3,10 @@
 # Get the directory this script is stored in
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Use thoughtbot's laptop script to set everything up
+"$DIR/laptop.sh"
+
+# Install lua
 "$DIR/install/lua_darwin.sh"
 # TODO: write code to install software on Darwin
 echo "Script for Darwin incomplete. Please install all the software manually."

4f10bd610caf9f1ff877a1373562df273e0bccb5
Stratus3D <admin@stratus3d.com>
2015-08-07 16:33:55 -0400

    Add more commands to debian setup.

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -3,5 +3,11 @@
 # Make apt-get calls non-interactive
 DEBIAN_FRONTEND=noninteractive
 
+# Tools I need
 sudo apt-get -y install git
 sudo apt-get -y install zsh
+sudo apt-get -y install tmux
+
+# ctags
+sudo apt-get install silversearcher-ag
+sudo apt-get -y install exuberant-ctags

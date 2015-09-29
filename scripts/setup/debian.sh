5681b31e2e09e965f9c760903ab7e8a3ae5d66a0
Stratus3D <admin@stratus3d.com>
2015-09-29 07:28:20 -0400

    More work on the debian setup script.

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -21,17 +21,16 @@ sudo apt-get -y install irssi
 sudo apt-get -y install thunderbird
 #sudo apt-get -y install chrome # chrome isn't available
 # Install hipchat
-sudo su
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
       /etc/apt/sources.list.d/atlassian-hipchat.list
 wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
-apt-get update
-apt-get install hipchat
+sudo apt-get update
+sudo apt-get -y install hipchat
 
 # Flux for lighting
 sudo add-apt-repository ppa:kilian/f.lux
 sudo apt-get update
-sudo apt-get install fluxgui
+sudo apt-get -y install fluxgui
 # TODO: Add flux to list of startup applications
 
 # Love 2D for game development

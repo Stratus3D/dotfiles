b401fc8b4b1e0cf693fa747d0bbdf66e4cb4b751
Stratus3D <admin@stratus3d.com>
2015-10-09 07:22:49 -0400

    Create first version of the generate_gitconfig.sh script.

---
+++ scripts/generate_gitconfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -
+###############################################################################
+# This script generates the .gitconfig based off the gitconfig file in this
+# repository and the mixins/gitconfig.custom file if it exists.
+###############################################################################
+dotfiles=$HOME/dotfiles               # dotfiles directory
+gitconfig=$dotfiles/gitconfig         # gitconfig source file
+destination=$HOME/.gitconfig          # gitconfig destination file
+custom_gitconfig=$dotfiles/mixins/gitconfig.custom # custom gitconfig
+
+if [ -f $custom_gitconfig ]; then
+    # Append custom gitconfig onto gitconfig
+    cat $gitconfig $custom_gitconfig > $destination
+else
+    # Just copy original gitconfig
+    cp $gitconfig $destination
+fi

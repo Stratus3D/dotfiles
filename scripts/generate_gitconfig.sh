ed9250882940c63bbdbdd81a12c9595051dd18d7
Stratus3D <admin@stratus3d.com>
2015-10-12 06:50:02 -0400

    Complete the generate_gitconfig.sh script.

--- scripts/generate_gitconfig.sh
+++ scripts/generate_gitconfig.sh
@@ -7,6 +7,11 @@ dotfiles=$HOME/dotfiles               # dotfiles directory
 gitconfig=$dotfiles/gitconfig         # gitconfig source file
 destination=$HOME/.gitconfig          # gitconfig destination file
 custom_gitconfig=$dotfiles/mixins/gitconfig.custom # custom gitconfig
+olddir=$HOME/dotfiles_old
+
+if [ -f $destination ]; then
+    mv $destination $olddir
+fi
 
 if [ -f $custom_gitconfig ]; then
     # Append custom gitconfig onto gitconfig

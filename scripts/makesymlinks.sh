a46b476c4ab45bbafa4f5c0f9b6d71404d548e6d
Trevor Brown <admin@stratus3d.com>
2015-10-15 08:54:07 -0400

    Add agignore to makesymlinks.sh script.

--- scripts/makesymlinks.sh
+++ scripts/makesymlinks.sh
@@ -16,7 +16,7 @@ dotfiles=$HOME/dotfiles               # dotfiles directory
 olddir=$HOME/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="vimrc vim zshrc bashrc tmux.conf gitignore_global ackrc ctags
-screenrc jshintrc rsync-exclude tool-versions"
+screenrc jshintrc rsync-exclude tool-versions agignore"
 
 # Remove everything in dotfiles_old
 rm -rf $olddir

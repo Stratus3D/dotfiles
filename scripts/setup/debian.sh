f747d518c18ccea7ce476dffe957cffba9d79980
Stratus3D <admin@stratus3d.com>
2015-12-02 08:12:35 -0500

    Fix hipchat install commands.

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -20,9 +20,11 @@ sudo apt-get -y install rbenv
 sudo apt-get -y install irssi
 sudo apt-get -y install thunderbird
 #sudo apt-get -y install chrome # chrome isn't available
+
 # Install hipchat
+sudo su
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
-      /etc/apt/sources.list.d/atlassian-hipchat.list
+    /etc/apt/sources.list.d/atlassian-hipchat.list
 wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
 sudo apt-get update
 sudo apt-get -y install hipchat

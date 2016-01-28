195fb42db19e0b3267fe93f7cc2f34437d09b29f
Trevor Brown <admin@stratus3d.com>
2016-01-28 08:57:53 -0500

    Remove nvm from setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -72,9 +72,6 @@ elif [[ "$unamestr" == 'Linux' ]]; then
     "$DIR/setup/linux.sh" 1>&1 2>&1
 fi
 
-# Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
-
 ############################
 # Install asdf for version management
 ############################

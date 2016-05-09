e86541de455a5aeeb7239b94e685ed8662efa587
Trevor Brown <admin@stratus3d.com>
2016-05-09 09:19:55 -0400

    Update the prax install script.

--- scripts/setup/install/prax.sh
+++ scripts/setup/install/prax.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -
 #
+# Build and install prax
+#
 # Usage ./prax.sh
 #
-# TODO: Complete this script
 
-sudo git clone git://github.com/ysbaddaden/prax.git /opt/prax
-cd /opt/prax/
-./bin/prax install
+sudo git clone git://github.com/ysbaddaden/prax.git /opt/prax || exit 1
+cd /opt/prax/ || exit 1
+./bin/prax install || exit 1

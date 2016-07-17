bd65b7c09a4cc18ddb3e029b2ecde37b34d652c8
Trevor Brown <admin@stratus3d.com>
2016-07-17 17:58:22 -0400

    Correct comment in debian setup script.

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -83,7 +83,7 @@ sudo apt-get -y install inkscape
 # Visualization library
 sudo apt-get -y install graphviz
 
-# Needed for ruby 2.2.0
+# Needed for Ruby and PostgreSQL
 sudo apt-get -y install libffi-dev autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 # Needed for capybara
 sudo apt-get -y install libqt4-dev libqtwebkit-dev

d1ecd581cd79b3716ef826722e859f6f7a51028e
Trevor Brown <admin@stratus3d.com>
2016-08-22 10:24:59 -0400

    Symlink bats to the bin directory so it's on the path.

--- scripts/setup/install/bats.sh
+++ scripts/setup/install/bats.sh
@@ -5,8 +5,10 @@
 # Usage ./prax.sh
 
 INSTALL_DIR=$HOME/lib
+BATS_BIN_DIR=$INSTALL_DIR/bats/bin
 
 cd $INSTALL_DIR
 git clone https://github.com/sstephenson/bats.git
 
-# TODO: Symlink each file in bats/bin/ to ~/bin
+# Symlink bats to the bin directory so it's on the path
+ln -s $BATS_BIN_DIR/bats $HOME/bin/bats

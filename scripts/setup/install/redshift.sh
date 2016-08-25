cf7b3cb0c235b3f1b799a85f93326cb578aabb7a
Stratus3D <admin@stratus3d.com>
2016-08-25 19:05:56 -0400

    Complete the redshift install script.

--- scripts/setup/install/redshift.sh
+++ scripts/setup/install/redshift.sh
@@ -4,10 +4,11 @@
 #
 # Usage ./redshift.sh
 #
-set -x 
+
 INSTALL_DIR=$HOME/lib
 BUILD_DIR=$INSTALL_DIR/redshift
 RELEASE=v1.11
+BIN_DIR=$BUILD_DIR/root/bin
 
 cd $INSTALL_DIR
 git clone git@github.com:jonls/redshift.git --branch $RELEASE $BUILD_DIR || exit 1
@@ -17,3 +18,10 @@ cd $BUILD_DIR || exit 1
 ./configure --enable-gui --prefix=$BUILD_DIR/root --with-systemduserunitdir=$HOME/.config/systemd/user || exit 1
 make || exit 1
 make install || exit 1
+
+# Allow the binaries to be executed
+chmod +x $BIN_DIR/redshift $BIN_DIR/redshift-gtk
+
+# Symlink redshift to the bin directory so it's on the path
+ln -s $BIN_DIR/redshift $HOME/bin/redshift || exit 1
+ln -s $BIN_DIR/redshift-gtk $HOME/bin/redshift-gtk || exit 1

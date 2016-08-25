76afcf3bebb59e86a10d7750d1551c86b943c3d9
Stratus3D <admin@stratus3d.com>
2016-08-24 20:11:21 -0400

    Make the bats.sh install script more strict.

--- scripts/setup/install/bats.sh
+++ scripts/setup/install/bats.sh
@@ -7,8 +7,8 @@
 INSTALL_DIR=$HOME/lib
 BATS_BIN_DIR=$INSTALL_DIR/bats/bin
 
-cd $INSTALL_DIR
-git clone https://github.com/sstephenson/bats.git
+cd $INSTALL_DIR || exit 1
+git clone https://github.com/sstephenson/bats.git || exit 1
 
 # Symlink bats to the bin directory so it's on the path
-ln -s $BATS_BIN_DIR/bats $HOME/bin/bats
+ln -s $BATS_BIN_DIR/bats $HOME/bin/bats || exit 1

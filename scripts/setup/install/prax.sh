b89c6c41455506fb45cf4a538edb6b0b46ab6751
Stratus3D <admin@stratus3d.com>
2016-09-05 10:13:14 -0400

    Add asdf to prax install script.

--- scripts/setup/install/prax.sh
+++ scripts/setup/install/prax.sh
@@ -5,6 +5,11 @@
 # Usage ./prax.sh
 #
 
-sudo git clone git://github.com/ysbaddaden/prax.git /opt/prax || exit 1
-cd /opt/prax/ || exit 1
+PRAX_LOCATION=/opt/prax/
+
+sudo git clone git://github.com/ysbaddaden/prax.git $PRAX_LOCATION || exit 1
+cd $PRAX_LOCATION || exit 1
 ./bin/prax install || exit 1
+
+# Make Ruby available to prax
+ln -s $HOME/.tool-versions $PRAX_LOCATION/.tool-versions || exit 1

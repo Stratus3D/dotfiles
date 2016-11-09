5e8ee6b5234ab961fa40c9f3c600a5bdf17040f3
Trevor Brown <admin@stratus3d.com>
2016-11-08 19:09:32 -0500

    Add osxfuse install script.

---
+++ scripts/setup/install/osxfuse.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\t\n' # Stricter IFS settings
+ORIGINAL_IFS=$IFS
+
+# Download osxfuse
+curl -OL https://github.com/osxfuse/osxfuse/releases/download/osxfuse-3.5.3/osxfuse-3.5.3.dmg
+
+# Download sshfs
+curl -OL https://github.com/osxfuse/sshfs/releases/download/osxfuse-sshfs-2.5.0/sshfs-2.5.0.pkg
+
+# TODO: Figure out how to install the dmg and the pkg

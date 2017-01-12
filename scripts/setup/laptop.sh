a177d7055ceea8804c153ad1e11a0d0cf4aafe31
Trevor Brown <admin@stratus3d.com>
2017-01-12 15:44:54 -0500

    Remove creation of ~/.bin directory from laptop.sh script.

--- scripts/setup/laptop.sh
+++ scripts/setup/laptop.sh
@@ -35,16 +35,9 @@ trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
 
 set -e
 
-if [ ! -d "$HOME/.bin/" ]; then
-  mkdir "$HOME/.bin"
-fi
-
 if [ ! -f "$HOME/.zshrc" ]; then
   touch "$HOME/.zshrc"
 fi
-
-# shellcheck disable=SC2016
-append_to_zshrc 'export PATH="$HOME/.bin:$PATH"'
 
 case "$SHELL" in
   */zsh) : ;;

5940955d24c65a49bb20a719c3502268b41ed146
Stratus3D <admin@stratus3d.com>
2017-02-10 20:48:30 -0500

    Add golang to debian setup script.

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -120,6 +120,9 @@ sudo apt-get -y install blender
 # Install Scribus for publishing
 sudo apt-get -y install scribus
 
+# Golang, for puma-dev
+sudo apt-get -y install golang
+
 # Misc other dependencies
 sudo apt-get -y install ncftp python-paramiko python-pycryptopp lftp python-boto python-dev librsync-dev
 

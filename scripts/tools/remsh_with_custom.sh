6b8fe7e41e88aa4dd3a203aa61bde48f0aa640bb
Trevor Brown <admin@stratus3d.com>
2017-03-14 10:00:16 -0400

    Check in remsh_with_custom.sh script.

---
+++ scripts/tools/remsh_with_custom.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+usage() {
+    cat <<EOF
+$(basename $0) [target] [erlang-cookie]
+
+Provides an easy way of running an Erlang remote shell (remsh).
+EOF
+}
+
+target="${1:-}"
+erlang_cookie="${2:-}"
+short_name="$( id -un )_$( date +%s )"
+
+if [[ -z "$target" ]]
+then
+    echo "ERROR: no node name specified"
+    usage
+    exit 1
+fi
+
+if [[ -z "$erlang_cookie" ]]
+then
+    echo "ERROR: no Erlang cookie specified"
+    usage
+    exit 1
+fi
+
+if echo $target | grep -q @ 2>/dev/null
+then
+    remote_node=$target
+else
+    remote_node="$target@$( hostname -s )"
+fi
+
+# Show debug info when running the erl command
+set -x
+
+# Evaluate the patch_remote call in the local shell before the remsh is started
+# so the remsh starts with the module already loaded
+erl -sname "$short_name" -setcookie "$erlang_cookie" -hidden -remsh $remote_node -pa $HOME/dotfiles/erlang/ -eval 'patch_remote:with_module(user_default, init:get_arguments()).'

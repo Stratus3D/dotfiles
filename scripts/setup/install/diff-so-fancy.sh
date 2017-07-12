1f23ebd71ab4416adda46b7d165ae6e242d89d84
Trevor Brown <admin@stratus3d.com>
2017-07-12 10:16:11 -0400

    Add diff-so-fancy install script.

---
+++ scripts/setup/install/diff-so-fancy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install diff-so-fancy for better Git diffs on the command line.
+# https://github.com/so-fancy/diff-so-fancy
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+# Create temp directory for the build
+TEMP_DIR=$(mktemp -dt "$(basename $0).XXXXXX") || exit 1
+echo "Created temp directory $TEMP_DIR"
+cd $TEMP_DIR
+
+# Download and unarchive
+git clone https://github.com/so-fancy/diff-so-fancy.git
+cd diff-so-fancy
+
+# Place diff-so-fancy on path
+cp diff-so-fancy $HOME/bin
+cp -r third_party $HOME/bin
+
+# Remove temp directory
+rm -rf $TEMP_DIR

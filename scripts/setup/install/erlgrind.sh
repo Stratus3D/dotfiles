3337ec4e0b7269b3b9b2019d115a772ee52d35bc
Trevor Brown <admin@stratus3d.com>
2017-08-17 09:04:35 -0400

    Complete erlgrind install script.

--- scripts/setup/install/erlgrind.sh
+++ scripts/setup/install/erlgrind.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash -
+#!/usr/bin/env bash
 #
-# Build the erlgrind Erlang library
+# Build and install the erlgrind Erlang library
 #
 # Usage ./erlgrind.sh
 #
@@ -8,21 +8,25 @@
 # Unoffical Bash "strict mode"
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
+#ORIGINAL_IFS=$IFS
 IFS=$'\t\n' # Stricter IFS settings
-ORIGINAL_IFS=$IFS
 
 ERLGRIND_LIB_DIR="$ERL_LIBS/erlgrind"
+BIN_DIR="$HOME/bin"
 
 echo "Building the erlgrind Erlang library"
 
-cd $ERL_LIBS || exit 1
+cd "$ERL_LIBS" || exit 1
 
 if [ -d "$ERLGRIND_LIB_DIR" ]; then
     echo "Directory $ERLGRIND_LIB_DIR already exists. Unable to install erlgrind"
     exit 1
 else
+    # Clone down the repo
     git clone git@github.com:isacssouza/erlgrind.git
-    # TODO: Figure out how to put the src/erlgrind script in the path
 
-    echo "Successfully built the observer_cli"
+    # Copy the erlgrind script the bin dir
+    cp erlgrind/src/erlgrind "$BIN_DIR"
+
+    echo "Installed erlgrind"
 fi

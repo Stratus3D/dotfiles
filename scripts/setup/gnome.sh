71aa794550ead174860748a490d4a948842e3262
Stratus3D <admin@stratus3d.com>
2017-08-27 16:04:08 -0400

    Add more settings changes to gnome.sh.

--- scripts/setup/gnome.sh
+++ scripts/setup/gnome.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -
 
+# Configure Gnome 3 the way I like it
+
+# Special screenshots directory
 gsettings set org.gnome.gnome-screenshot auto-save-directory "$HOME/Screenshots/"
+
+# Turn off animations to improve performance
+gsettings set org.gnome.desktop.interface enable-animations false

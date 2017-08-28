349fb02ef53e4f9be61635c3b68c76bac63bfefa
Trevor Brown <admin@stratus3d.com>
2017-08-28 08:43:02 -0400

    Add quick-stats install script.

---
+++ scripts/setup/install/git-quick-stats.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install git-quick-stats for Git repo statistics.
+# https://github.com/arzzen/git-quick-stats
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+# Create temp directory for the build
+TEMP_DIR=$(mktemp -dt "$(basename $0).XXXXXX") || exit 1
+echo "Created temp directory $TEMP_DIR"
+cd $TEMP_DIR
+
+# Download and unarchive
+git clone https://github.com/arzzen/git-quick-stats.git
+cd git-quick-stats
+
+# Install
+make install
+
+# Remove temp directory
+rm -rf $TEMP_DIR

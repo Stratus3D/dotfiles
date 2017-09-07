3484fe484bd6d61bc30355e96592bc13d31b532e
Trevor Brown <admin@stratus3d.com>
2017-09-07 09:09:54 -0400

    Update observer_cli install script so it installs the binary as well.

--- scripts/setup/install/observer_cli.sh
+++ scripts/setup/install/observer_cli.sh
@@ -12,18 +12,28 @@ IFS=$'\t\n' # Stricter IFS settings
 ORIGINAL_IFS=$IFS
 
 OBSERVER_CLI_LIB_DIR="$ERL_LIBS/observer_cli"
+BIN_DIR="$HOME/bin"
 
 echo "Building the observer_cli Erlang library"
 
-cd $ERL_LIBS || exit 1
+cd $ERL_LIBS
 
 if [ -d "$OBSERVER_CLI_LIB_DIR" ]; then
     echo "Directory $OBSERVER_CLI_LIB_DIR already exists. Unable to install observer_cli"
     exit 1
 else
-    git clone git@github.com:zhongwencool/observer_cli.git || exit 1
-    cd $OBSERVER_CLI_LIB_DIR || exit 1
-    make || exit 1
+    git clone git@github.com:zhongwencool/observer_cli.git $OBSERVER_CLI_LIB_DIR
+    cd $OBSERVER_CLI_LIB_DIR
+
+    # Generate an escript
+    rebar3 escriptize
+    cp _build/default/bin/observer_cli $BIN_DIR
+
+    # Compile the source code and move the beam files into the ebin dir so
+    # Erlang can find them
+    rebar3 compile
+    cp -R _build/default/deps/observer_cli/ebin .
+    cp -R _build/default/deps/recon/ebin .
 
     echo "Successfully built the observer_cli"
 fi

4e6972137d3cf5a17fb5f86126af1f10e91109f9
Trevor Brown <admin@stratus3d.com>
2017-09-15 09:11:04 -0400

    Create sshmux function.

---
+++ scripts/functions/sshmux.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# I wish I could put this in ssh/config but it doesn't appear to be possible
+
+function sshmux {
+    # Pass all arguments directly to `ssh` and add the tmux command
+    ssh $@ -t 'tmux has-session && exec tmux attach || exec tmux; tmux showenv -s;'
+}

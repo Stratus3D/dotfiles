f55f0c878450cc49d583a6e386a1321eec4d12dc
Trevor Brown <admin@stratus3d.com>
2017-11-15 10:19:55 -0500

    Add other packages for working with CSV and XML on the command line.

--- scripts/setup/packages.sh
+++ scripts/setup/packages.sh
@@ -5,6 +5,9 @@
 # flake8 for linting
 python -m pip install flake8
 
+# csvkit for CSV utilities
+pip install csvkit
+
 # Node.JS Packages
 
 # fb-messenger-cli for messaging
@@ -12,3 +15,6 @@ npm install -g fb-messenger-cli
 
 # Install jslint for linting
 npm install -g jslint
+
+# Install xml2json tool
+npm install -g xml2json-command

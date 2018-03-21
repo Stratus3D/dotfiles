d552c5611e717245a44ffa48fb2657b1bbf5dc38
Trevor Brown <admin@stratus3d.com>
2018-03-21 17:48:35 -0400

    Switch from the silver search to ripgrep.

--- scripts/setup/darwin.sh
+++ scripts/setup/darwin.sh
@@ -143,9 +143,8 @@ brew update
 
 brew_install_or_upgrade 'zsh'
 brew_install_or_upgrade 'git'
-brew_launchctl_restart 'postgresql'
 brew_launchctl_restart 'redis'
-brew_install_or_upgrade 'the_silver_searcher'
+brew_install_or_upgrade 'ripgrep'
 brew_install_or_upgrade 'vim'
 brew_install_or_upgrade 'ctags'
 brew_install_or_upgrade 'tmux'

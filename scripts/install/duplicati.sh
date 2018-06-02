094ca9a774f9a1efe1c3997480c1f291f8c2c50f
Stratus3D <admin@stratus3d.com>
2018-06-02 14:52:49 -0400

    Create duplicati install script.

---
+++ scripts/install/duplicati.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Install duplicati on debian
+#
+# Usage ./duplicati.sh
+#
+
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+# Create temp directory for the build
+TEMP_DIR=$(mktemp -dt "$(basename $0).XXXXXX")
+echo "Created temp directory $TEMP_DIR"
+cd $TEMP_DIR
+
+DEB_FILENAME="duplicati_2.0.3.3-1_all.deb"
+
+# Download the deb file
+wget -O "$DEB_FILENAME" "https://updates.duplicati.com/beta/$DEB_FILENAME"
+
+# Install the deb file
+sudo dpkg -i $DEB_FILENAME
+
+# Remove the temp dir
+rm -r $TEMP_DIR

5dd7182b0721dd9767c97ac0bff851b3da14971f
Stratus3D <admin@stratus3d.com>
2018-08-09 18:16:57 -0400

    Update bats install script so it works even when bats is already installed.

--- scripts/install/bats.sh
+++ scripts/install/bats.sh
@@ -14,7 +14,9 @@ INSTALL_DIR=$HOME/lib
 BATS_BIN_DIR=$INSTALL_DIR/bats/bin
 
 cd $INSTALL_DIR || exit 1
+# Remove the directory if it already exists before cloning
+rm -rf bats
 git clone https://github.com/sstephenson/bats.git
 
 # Symlink bats to the bin directory so it's on the path
-ln -s $BATS_BIN_DIR/bats $HOME/bin/bats
+ln -sf $BATS_BIN_DIR/bats $HOME/bin/bats

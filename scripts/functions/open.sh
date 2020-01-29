3f9a61c78e4630b6b84b5521e3d1b826b5737a2b
Stratus3D <admin@stratus3d.com>
2020-01-28 21:18:47 -0500

    Add in generic open_ticket function

--- scripts/functions/open.sh
+++ scripts/functions/open.sh
@@ -30,3 +30,12 @@ function duc() {
     search_string "$@"
     open "http://www.duckduckgo.com?q=$(search_string "$@")"
 }
+
+function open_ticket() {
+  if [ -n "$BASE_TICKET_URL" ]; then
+    open "$BASE_TICKET_URL$1"
+  else
+    echo "BASE_TICKET_URL is not set"
+    return 1
+  fi
+}

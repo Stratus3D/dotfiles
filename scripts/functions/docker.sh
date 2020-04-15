f38fd3e176955909c1b4806cdc88116cfe327d05
Stratus3D <admin@stratus3d.com>
2020-04-15 16:30:47 -0400

    Update docker_clean function to remove unused networks and volumes

--- scripts/functions/docker.sh
+++ scripts/functions/docker.sh
@@ -2,8 +2,18 @@
 
 docker_clean() {
     # Remove stopped containers
+    echo "Removing stopped containers..."
     docker ps -aq --no-trunc | xargs docker rm
 
     # Remove dangling/untagged images
+    echo "Removing untagged images..."
     docker images --quiet --filter=dangling=true | xargs docker rmi
+
+    # Remove unused docker networks
+    echo "Removing unused Docker networks..."
+    docker network prune -f
+
+    # Remove unused docker volumes
+    echo "Removing unused Docker volumes..."
+    docker network prune -f
 }

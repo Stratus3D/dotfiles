0a5704503f0af25411c785aeddb9694e667cf849
Stratus3D <admin@stratus3d.com>
2020-04-21 19:21:27 -0400

    Add setting for gnome desktop background color

--- scripts/setup/gnome.sh
+++ scripts/setup/gnome.sh
@@ -7,3 +7,6 @@ gsettings set org.gnome.gnome-screenshot auto-save-directory "$HOME/Screenshots/
 
 # Turn off animations to improve performance
 gsettings set org.gnome.desktop.interface enable-animations false
+
+# Set desktop background color
+gsettings set org.gnome.desktop.background primary-color '#221a15'

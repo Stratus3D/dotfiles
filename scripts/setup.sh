afe4d868be06c454619d9074834da758f470a169
Stratus3D <admin@stratus3d.com>
2020-06-18 13:15:16 -0400

    Add code to create clients directory to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -63,6 +63,7 @@ mkdir -p $HOME/audio
 mkdir -p $HOME/audio/podcasts # For podcast storage
 mkdir -p $HOME/audio/music # For music storage
 mkdir -p $HOME/servers # For remote server mounts
+mkdir -p $HOME/clients # Files for third parties - companies, friends, etc...
 mkdir -p $HOME/.psql # psql history directory
 
 ###############################################################################

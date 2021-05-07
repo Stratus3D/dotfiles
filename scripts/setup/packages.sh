abd241d3bba7b61492df36c2d6e94fbd7ea8b0f1
Stratus3D <admin@stratus3d.com>
2021-05-07 19:44:06 -0400

    Update docs in packages.sh

--- scripts/setup/packages.sh
+++ scripts/setup/packages.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env bash
 
+# Unoffical Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
+# This script contains calls to package managers that should be the same across
+# different operating systems. Since this script can be invoked on any
+# operating system, we invoke it from the main setup.sh when setting up a new
+# computer
+
 # Ruby Packages
 ###############################################################################
 

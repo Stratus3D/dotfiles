9851f92e0e258a817f06b1f4e9e3bd02f3f62a72
Stratus3D <admin@stratus3d.com>
2021-05-19 15:18:16 -0400

    Remove skypeforlinux package from debian setup script

--- scripts/setup/debian.sh
+++ scripts/setup/debian.sh
@@ -84,9 +84,6 @@ install_or_upgrade fonts-inconsolata
 # For documents
 install_or_upgrade fonts-cmu
 
-# Skype
-install_or_upgrade skypeforlinux
-
 # Ungoogled Chromium (Ubuntu Focal)
 echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
 curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null

85c519386211ab7bdcb1a455bf037e22a1ad9c98
Stratus3D <admin@stratus3d.com>
2021-09-28 15:55:35 -0400

    Add comments to iex.exs file

--- iex.exs
+++ iex.exs
@@ -1,3 +1,4 @@
+# Configure IEx shell
 IEx.configure(
   history_size: -1,
   colors: [ eval_result: [ :cyan, :bright ] ],
@@ -7,3 +8,5 @@ IEx.configure(
     width: 80
   ]
 )
+
+# Put any code here that should be executed when iex is started

d9fd640e0135ca84464798535fd73e9c9c231306
Trevor Brown <admin@stratus3d.com>
2023-07-18 19:52:52 -0400

    Simplify makesymlinks.sh script

--- scripts/makesymlinks.sh
+++ scripts/makesymlinks.sh
@@ -1,14 +1,15 @@
-#!/bin/bash -
+#!/usr/bin/env bash
+
+# Unofficial Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+#ORIGINAL_IFS=$IFS
+IFS=$'\t\n' # Stricter IFS settings
+
 ###############################################################################
 # This script creates symlinks from the home directory to any desired dotfiles
 # in ~/dotfiles
 ###############################################################################
-
-set -u # Prevent unset variables
-set -e # Stop on an error
-set -o pipefail # Pipe exit code should be non-zero when a command in it fails
-IFS=$'\t\n' # Stricter IFS settings
-ORIGINAL_IFS=$IFS
 
 ###############################################################################
 # Variables
@@ -19,9 +20,6 @@ border="====="
 
 # Dotfiles directory
 dotfiles=$HOME/dotfiles
-
-# Old dotfiles backup directory
-olddir=$HOME/dotfiles_old
 
 # List of space separated files/folders to symlink in homedir (these are the typical "dotfiles")
 files="vimrc vim zshrc bashrc tmux.conf gitignore_global ctags screenrc \
@@ -42,16 +40,16 @@ print_heading() {
 make_dir_if_missing() {
     local directory=$1
 
-    if [ ! -d $directory ]; then
-        mkdir $directory
+    if [ ! -d "$directory" ]; then
+        mkdir "$directory"
     fi
 }
 
 remove_dir_if_exists() {
     local directory=$1
 
-    if [ -d $directory ]; then
-        rm -rf $directory
+    if [ -d "$directory" ]; then
+        rm -rf "$directory"
     fi
 }
 
@@ -59,8 +57,8 @@ symlink_file_if_missing() {
     local source=$1
     local destination=$2
 
-    if [ ! -f $destination ]; then
-        ln -nfs $source $destination
+    if [ ! -f "$destination" ]; then
+        ln -nfs "$source" "$destination"
     fi
 }
 
@@ -68,83 +66,75 @@ create_or_replace_symlink() {
     local source=$1
     local destination=$2
 
-    if [ -L $destination ]; then
-        rm $destination
+    if [ -L "$destination" ]; then
+        rm "$destination"
     fi
 
-    ln -nfs $source $destination
+    ln -nfs "$source" "$destination"
 }
 
-symlink_and_save_original() {
+symlink() {
     local source=$1
     local destination=$2
-    local backup_dir=$3
 
-    if [[ -L $destination ]]; then
-        echo "Removing $destination symlink"
-        rm $destination;
-    else
-        if [[ -e $destination ]]; then
-            echo "Moving existing $destination from $HOME to $backup_dir"
-            mv $destination $backup_dir;
-        fi
+    if [ -e "$destination" ] || [ -L "$destination" ]; then
+      echo "Removing $(basename "$destination") from $HOME"
+      rm "$destination";
     fi
-    echo "Creating symlink to $file in home directory."
+
     echo "Symlinking $destination -> $source"
     ln -s "$source" "$destination"
 }
 
-# Remove everything in dotfiles_old
-rm -rf $olddir
-
-# create dotfiles_old in homedir
-mkdir -p $olddir
-echo "Created $olddir for backup of any existing dotfiles in $HOME"
+print_heading "Linking Dotfiles"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create
 # symlinks
+ORIGINAL_IFS=$IFS
 IFS=$' '
 for file in $files; do
-  oldfile="$HOME/.$file"
-
-  symlink_and_save_original $dotfiles/$file $oldfile $olddir
+  symlink "$dotfiles/$file" "$HOME/.$file"
 done
 IFS=$ORIGINAL_IFS
 
 # Generate and copy gitconfig
-$dotfiles/scripts/generate_gitconfig.sh
+"$dotfiles/scripts/generate_gitconfig.sh"
 
 # setup default tmuxinator project
-make_dir_if_missing $HOME/.tmuxinator
+make_dir_if_missing "$HOME/.tmuxinator"
 
 # Create directory for Vim undo history
-make_dir_if_missing $dotfiles/vim/undodir
+make_dir_if_missing "$dotfiles/vim/undodir"
 
 # link the default tmuxinator project
-symlink_and_save_original $dotfiles/tmuxinator/default.yml \
-    $HOME/.tmuxinator/default.yml $olddir
+symlink "$dotfiles/tmuxinator/default.yml" "$HOME/.tmuxinator/default.yml"
 
 # Symlink the .erlang file
-symlink_and_save_original $dotfiles/erlang/erlang \
-    $HOME/.erlang $olddir
+symlink "$dotfiles/erlang/erlang" "$HOME/.erlang"
 
-# Symlink all the scripts in scripts/tools to the bin directory
-tool_scripts=$(find $dotfiles/scripts/tools -type f \( -perm -u=x \) -print)
+# Symlink all the executable scripts in scripts/tools to the bin directory
+tool_scripts="$(find "$dotfiles/scripts/tools" -type f \( -perm -u=x \) -print)"
 IFS=$'\n'
 for file in $tool_scripts; do
-    create_or_replace_symlink $file $HOME/bin
+    create_or_replace_symlink "$file" "$HOME/bin"
 done
 IFS=$ORIGINAL_IFS
 
 # Symlink all the scripts in scripts/git to the bin directory
-tool_scripts=$(find $dotfiles/scripts/git -type f \( -perm -u=x \) -print)
+tool_scripts="$(find "$dotfiles/scripts/git" -type f \( -perm -u=x \) -print)"
 IFS=$'\n'
 for file in $tool_scripts; do
-    create_or_replace_symlink $file $HOME/bin
+    create_or_replace_symlink "$file" "$HOME/bin"
 done
 IFS=$ORIGINAL_IFS
 
-# Download Vundle
+# Symlink hosts_manager hosts profiles directory
+hosts_dir="$HOME/.hosts"
+hosts_source_dir="$dotfiles/hosts_profiles"
+symlink "$hosts_source_dir" "$hosts_dir"
+
+# Download Vundle if not already downloaded
+print_heading "Installing Vundle"
 vundle_dir="$dotfiles/vim/bundle/Vundle.vim"
 if [ ! -d "$vundle_dir" ]; then
   git clone https://github.com/gmarik/Vundle.vim.git "$vundle_dir"
@@ -153,12 +143,7 @@ else
 fi
 
 # Install Vundle and all other plugins
-echo "Installing Vim plugins..."
+print_heading "Installing Vim Plugins"
 vim +PluginInstall +qall
 
-# Symlink hosts_manager hosts profiles directory
-hosts_dir="$HOME/.hosts"
-hosts_source_dir="$dotfiles/hosts_profiles"
-symlink_and_save_original $hosts_source_dir $hosts_dir $olddir
-
-print_heading "Linking complete!"
+print_heading "Linking and Vim Configuration Complete"

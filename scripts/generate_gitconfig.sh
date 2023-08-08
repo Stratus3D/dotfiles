f75c5692b5e0f9808e1d45b906094ed36964f685
Trevor Brown <admin@stratus3d.com>
2023-08-07 20:05:01 -0400

    Modernize generate_gitconfig script

--- scripts/generate_gitconfig.sh
+++ scripts/generate_gitconfig.sh
@@ -1,17 +1,24 @@
-#!/bin/bash -
-###############################################################################
+#!/usr/bin/env bash
+
+# Unofficial Bash "strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\t\n' # Stricter IFS settings
+
 # This script generates the .gitconfig based off the gitconfig file in this
 # repository and the mixins/gitconfig.custom file if it exists.
-###############################################################################
-dotfiles=$HOME/dotfiles               # dotfiles directory
-gitconfig=$dotfiles/gitconfig         # gitconfig source file
-destination=$HOME/.gitconfig          # gitconfig destination file
-custom_gitconfig=$dotfiles/mixins/gitconfig.custom # custom gitconfig
-olddir=$HOME/dotfiles_old
 
-if [ -f $destination ]; then
-    mv $destination $olddir
-fi
+# dotfiles directory
+dotfiles=$HOME/dotfiles
+
+# gitconfig source file
+gitconfig=$dotfiles/gitconfig
+
+# gitconfig destination file
+destination=$HOME/.gitconfig
+
+# custom gitconfig
+custom_gitconfig=$dotfiles/mixins/gitconfig.custom
 
 if [ -f $custom_gitconfig ]; then
     # Append custom gitconfig onto gitconfig

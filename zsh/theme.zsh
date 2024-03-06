2fc58cf5f55667605ac756ea6471bade2b538623
Trevor Brown <admin@stratus3d.com>
2024-03-06 16:01:47 -0500

    Working on improving zsh prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -41,16 +41,29 @@ function _prompt_char() {
   fi
 }
 
-bkg=white
+prompt_bg=white
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{blue}%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{${bkg}}%B%F{green}%}]"
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
+#ZSH_THEME_GIT_PROMPT_PREFIX=" [%{%B%F{blue}%}"
+#ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{${bkg}}%B%F{green}%}]"
+#ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
+#ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT='%{%f%k%b%}
-%{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E %{%K{${bkg}}%}%{$fg_bold[red]%}%(?..%?)%{%f%b%}%{%f%k%b%}
-$(_prompt_char)%{%K{${bkg}}%} %#%{%f%k%b%} '
+PROMPT='%(?.%F{14}⏺.%F{9}⏺)%f %2~ %# '
+# Show running background jobs, if any
+#
+# %{%f%k%b%u%}         - A reset. Stop using background color, foreground color, bold, and underline
+# %{%K{${prompt_bg}}%} - Set background color for prompt
+# - Display previous exit code. Use bold red if failed, otherwise green
+#
+PROMPT='%{%f%k%b%u%}\
+%{%K{${prompt_bg}}%}\
+%{%(?.%F{2}%?.%F{%{7}%?)%3G%}\
+%{%B%F{blue}%} %(1j.(%j).) %{%b%F{yellow}%K{${prompt_bg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E %{%K{${prompt_bg}}%}%{$fg_bold[red]%}%(?..%?)%{%f%b%}%{%f%k%b%}
+$(_prompt_char)%{%K{${prompt_bg}}%} %#%{%f%k%b%} '
+
+#PROMPT='%{%f%k%b%}
+#%{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E %{%K{${bkg}}%}%{$fg_bold[red]%}%(?..%?)%{%f%b%}%{%f%k%b%}
+#$(_prompt_char)%{%K{${bkg}}%} %#%{%f%k%b%} '
 
 # I no longer use the right prompt because I often forgot it was there
 #RPROMPT='%{%K{${bkg}}%}%{$fg_bold[red]%}%(?..%?)%{%f%b%} !%{%B%F{cyan}%}%!%{%f%k%b%}'

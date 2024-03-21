d339a09e215c481a4630c40ec2e087d68c2d8b2e
Trevor Brown <admin@stratus3d.com>
2024-03-21 14:21:23 -0400

    Add timer to zsh prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -42,12 +42,51 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{%f%k%b%K{${prompt_bg}}%B%F{green}%}]"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{%F{red}%}*%{%f%k%b%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
+# The code below was taken from the ohmyzsh timer plugin
+zmodload zsh/datetime
+
+__prompttimer_current_time() {
+  zmodload zsh/datetime
+  echo $EPOCHREALTIME
+}
+
+__prompttimer_format_duration() {
+  local mins=$(printf '%.0f' $(($1 / 60)))
+  local secs=$(printf "%.${TIMER_PRECISION:-1}f" $(($1 - 60 * mins)))
+  local duration_str=$(echo "${mins}m${secs}s")
+  local format="${TIMER_FORMAT:-%d}"
+  echo "${format//\%d/${duration_str#0m}}"
+}
+
+__prompttimer_save_time_preexec() {
+  __prompttimer_cmd_start_time=$(__prompttimer_current_time)
+}
+
+__prompttimer_save_timer_display_precmd() {
+  # set to default of empty string
+  __prompttimer_prev_cmd_duration=""
+  if [ -n "${__prompttimer_cmd_start_time}" ]; then
+    local cmd_end_time=$(__prompttimer_current_time)
+    local tdiff=$((cmd_end_time - __prompttimer_cmd_start_time))
+    unset __prompttimer_cmd_start_time
+    if [[ -z "${TIMER_THRESHOLD}" || ${tdiff} -ge "${TIMER_THRESHOLD}" ]]; then
+        local tdiffstr=$(__prompttimer_format_duration ${tdiff})
+        local cols=$((COLUMNS - ${#tdiffstr} - 1))
+        __prompttimer_prev_cmd_duration=" ${tdiffstr}"
+    fi
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook preexec __prompttimer_save_time_preexec
+add-zsh-hook precmd __prompttimer_save_timer_display_precmd
+
 # Show running background jobs, if any
 #
 # %{%f%k%b%u%}         - A reset. Stop using background color, foreground color, bold, and underline
 # %{%K{${prompt_bg}}%} - Set background color for prompt
 # %(?.%F{green}%?.%B%F{%{red}%}%?) - Display previous exit code. Use bold red if failed, otherwise green
-# - Display the duration of the previous command in yellow
+# %{%F{yellow}%}$__timer_prev_cmd_duration - Display the duration of the previous command in yellow
 # %{%B%F{blue}%}%(1j. (%j).) - Display the number jobs if one or more
 # %{%B%F{green}%}$(git_prompt_info) - Show Git info if inside a Git repository
 # %{%b%F{magenta}%}%~   - Display the current directory in magenta bold font
@@ -55,11 +94,10 @@ ZSH_THEME_GIT_PROMPT_CLEAN=""
 # %#                   - Display # prompt if root, and % if not
 # %{%f%k%b%u%}         - The reset again.
 
-# TODO: Populate duration with actual time. Replace `ms` with actual millisecond symbol
 PROMPT='%{%f%k%b%u%}\
 %{%K{${prompt_bg}}%}\
 %(?.%F{green}%?.%B%F{%{red}%}%?)\
- %{%F{yellow}%}1m23s45ms\
+%{%b%F{yellow}%}$__prompttimer_prev_cmd_duration\
 %{%B%F{blue}%}%(1j. (%j).)\
 %{%B%F{green}%}$(git_prompt_info)\
  %{%b%F{cyan}%}%n\
